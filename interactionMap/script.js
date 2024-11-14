33ffb6f18bb8aee9a34a27a9639f8b6b78e8ba6c
unknown <marcellbanol@hotmail.com>
2024-11-14 19:25:50 +0100

    ça fait beacoup là non ?

--- interactionMap/script.js
+++ interactionMap/script.js
@@ -9,68 +9,25 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Sélection de la zone d'information
 const infoBox = document.getElementById('informationDonnee');
 
-// Variable pour stocker le département sélectionné
-let selectedLayer = null;
-
-// Structure pour stocker les données des établissements par département
-const departmentStats = {};
+// Variable pour stocker les marqueurs et éviter d'ajouter plusieurs fois
 let establishmentMarkers = [];
+
+// Structure pour stocker le nombre d'établissements publics et privés par département
+const departmentStats = {};
 
 // Fonction pour mettre à jour les statistiques par département
 function updateDepartmentStats(etablissement) {
-    const deptName = etablissement.libelle_departement; // Assurez-vous que chaque établissement a un champ 'libelle_departement'
-    const secteur = etablissement.secteur_public_prive_libe; // "Public" ou "Privé sous contrat"
+    const deptName = etablissement.libelle_departement;
 
-    // Vérifier que les valeurs sont correctes
-    console.log('Département:', deptName, 'Secteur:', secteur);
-
-    // Initialisation des statistiques pour le département si elles n'existent pas
     if (!departmentStats[deptName]) {
         departmentStats[deptName] = { public: 0, prive: 0 };
     }
 
-    // Incrémentation du nombre d'établissements public/privé sous contrat
-    if (secteur === 'PUBLIC') {
+    if (etablissement.secteur === "PUBLIC") {
         departmentStats[deptName].public += 1;
-    } else if (secteur === 'PRIVE SOUS CONTRAT') {
+    } else if (etablissement.secteur === "PRIVE SOUS CONTRAT") {
         departmentStats[deptName].prive += 1;
     }
-}
-
-// Fonction pour afficher les marqueurs d'un département
-function showMarkersForDepartment(departmentName) {
-    // Effacer les marqueurs précédemment ajoutés
-    establishmentMarkers.forEach(marker => marker.remove());
-    establishmentMarkers = []; // Réinitialiser le tableau des marqueurs
-
-    // Ajouter les marqueurs pour le département sélectionné
-    data.forEach(etablissement => {
-        if (etablissement.libelle_departement === departmentName) {
-            const { latitude, longitude, appellation_officielle, secteur_public_prive_libe, effectif_total } = etablissement;
-            
-            // Crée un marqueur pour chaque établissement
-            const marker = L.marker([latitude, longitude]).addTo(map);
-            establishmentMarkers.push(marker); // Ajouter le marqueur au tableau
-
-            // Ajoute un événement de survol pour afficher les informations
-            marker.on('mouseover', function () {
-                infoBox.innerHTML = `
-                    <b>${appellation_officielle}</b><br>
-                    Type : ${secteur_public_prive_libe}<br>
-                    Effectif Total : ${effectif_total}
-                `;
-            });
-
-            // Ajoute un événement de clic pour afficher plus d'informations
-            marker.on('click', function () {
-                infoBox.innerHTML = `
-                    <b>${appellation_officielle}</b><br>
-                    Type : ${secteur_public_prive_libe}<br>
-                    Effectif Total : ${effectif_total}
-                `;
-            });
-        }
-    });
 }
 
 // Chargement des données des établissements
@@ -81,100 +38,115 @@ fetch('effectifs-en-terminale-specialites-academie-versailles-2022(1).json')
         data.forEach(etablissement => {
             updateDepartmentStats(etablissement);
         });
+
+        // Fonction pour mettre en évidence les départements de l'Académie de Versailles
+        function highlightAcademy(feature) {
+            const departementAcademie = ["Yvelines", "Essonne", "Hauts-de-Seine", "Val-d'Oise"];
+            if (departementAcademie.includes(feature.properties.nom)) {
+                return {
+                    color: "black",
+                    weight: 2,
+                    fillColor: "blue",
+                    fillOpacity: 0.2
+                };
+            } else {
+                return {
+                    color: "black",
+                    weight: 1,
+                    fillColor: "gray",
+                    fillOpacity: 0.01
+                };
+            }
+        }
+
+        // Fonction pour afficher les marqueurs pour un département
+        function showMarkersForDepartment(departmentName) {
+            // Effacer les marqueurs précédemment ajoutés
+            establishmentMarkers.forEach(marker => marker.remove());
+            establishmentMarkers = []; // Réinitialiser le tableau des marqueurs
+
+            // Ajouter les marqueurs pour le département sélectionné
+            data.forEach(etablissement => {
+                if (etablissement.libelle_departement === departmentName) {
+                    const { latitude, longitude, appellation_officielle, secteur, effectif_total } = etablissement;
+                    
+                    // Crée un marqueur pour chaque établissement
+                    const marker = L.marker([latitude, longitude]).addTo(map);
+                    establishmentMarkers.push(marker); // Ajouter le marqueur au tableau
+
+                    // Ajoute un événement de survol pour afficher les informations
+                    marker.on('mouseover', function () {
+                        infoBox.innerHTML = `
+                            <b>${appellation_officielle}</b><br>
+                            Type : ${secteur}<br>
+                            Effectif Total : ${effectif_total}
+                        `;
+                    });
+
+                    // Ajoute un événement de clic pour afficher plus d'informations
+                    marker.on('click', function () {
+                        infoBox.innerHTML = `
+                            <b>${appellation_officielle}</b><br>
+                            Type : ${secteur}<br>
+                            Effectif Total : ${effectif_total}
+                        `;
+                    });
+                }
+            });
+        }
+
+        // Fonction pour changer le style au survol et afficher les informations dans le infoBox
+        function highlightFeature(e) {
+            const layer = e.target;
+            const deptName = layer.feature.properties.nom;
+            const stats = departmentStats[deptName] || { public: 0, prive: 0 };
+
+            // Afficher les informations du département dans la zone infoBox
+            infoBox.innerHTML = `
+                <b>Département :</b> ${deptName}<br>
+                <b>Établissements publics :</b> ${stats.public}<br>
+                <b>Établissements privés :</b> ${stats.prive}
+            `;
+        }
+
+        // Fonction pour rétablir le style initial et vider le infoBox
+        function resetHighlight(e) {
+            const layer = e.target;
+            geojson.resetStyle(layer);
+            infoBox.innerHTML = "Survolez un département pour voir les informations";
+        }
+
+        // Fonction pour zoomer sur le département lors d'un clic
+        function zoomToFeature(e) {
+            const layer = e.target;
+
+            // Zoomer sur le département
+            map.fitBounds(layer.getBounds());
+
+            // Récupère le nom du département
+            const deptName = layer.feature.properties.nom;
+
+            // Afficher les marqueurs pour le département sélectionné
+            showMarkersForDepartment(deptName);
+        }
+
+        // Fonction pour attacher les événements de survol, de clic et de popup à chaque département
+        function onEachFeature(feature, layer) {
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            });
+        }
+
+        // Chargement du fichier GeoJSON contenant tous les départements
+        const geojsonUrl = "https://france-geojson.gregoiredavid.fr/repo/regions/ile-de-france/departements-ile-de-france.geojson";
+
+        // Ajout du GeoJSON sur la carte avec le style conditionnel et événements
+        const geojson = new L.GeoJSON.AJAX(geojsonUrl, {
+            style: highlightAcademy,
+            onEachFeature: onEachFeature
+        }).addTo(map);
+
     })
     .catch(error => console.error("Erreur de chargement des données : ", error));
-
-// Fonction pour mettre en évidence les départements de l'Académie de Versailles
-function highlightAcademy(feature) {
-    const departementAcademie = ["Yvelines", "Essonne", "Hauts-de-Seine", "Val-d'Oise"];
-    
-    if (departementAcademie.includes(feature.properties.nom)) {
-        return {
-            color: "black",      // Couleur des frontières
-            weight: 2,
-            fillColor: "blue",   // Couleur de remplissage
-            fillOpacity: 0.2
-        };
-    } else {
-        return {
-            color: "black",
-            weight: 1,
-            fillColor: "gray", // Couleur de remplissage pour les autres départements
-            fillOpacity: 0.01
-        };
-    }
-}
-
-// Fonction pour changer le style au survol et afficher les informations dans le infoBox
-function highlightFeature(e) {
-    const layer = e.target;
-    const deptName = layer.feature.properties.nom;
-
-    // Récupère les statistiques des établissements publics et privés pour le département
-    const stats = departmentStats[deptName] || { public: 0, prive: 0 };
-
-    // Afficher les informations du département dans la zone infoBox
-    infoBox.innerHTML = `
-        <b>Département :</b> ${deptName}<br>
-        <b>Établissements publics :</b> ${stats.public}<br>
-        <b>Établissements privés :</b> ${stats.prive}
-    `;
-}
-
-// Fonction pour rétablir le style initial et vider le infoBox
-function resetHighlight(e) {
-    const layer = e.target;
-    if (layer !== selectedLayer) {  // S'assurer que le département sélectionné garde son style modifié
-        geojson.resetStyle(layer);
-    }
-    infoBox.innerHTML = "Survolez un département pour voir les informations";
-}
-
-// Fonction pour zoomer sur le département lors d'un clic et supprimer la surbrillance et la couleur de remplissage
-function zoomToFeature(e) {
-    const layer = e.target;
-
-    // Zoomer sur le département
-    map.fitBounds(layer.getBounds());
-
-    // Si un département est déjà sélectionné, réinitialiser son style
-    if (selectedLayer) {
-        geojson.resetStyle(selectedLayer);
-    }
-
-    // Définir le département cliqué comme sélectionné
-    selectedLayer = layer;
-
-    // Modifier le style du département sélectionné pour enlever la couleur de remplissage
-    selectedLayer.setStyle({
-        color: "black",
-        weight: 2,
-        fillColor: "none",  // Enlève la couleur de remplissage
-        fillOpacity: 0      // Rendre le remplissage complètement transparent
-    });
-
-    // Mettre à jour l'infoBox avec le nom du département sélectionné
-    const deptName = layer.feature.properties.nom;
-    infoBox.innerHTML = `<b>Département :</b> ${deptName}`;
-
-    // Afficher les marqueurs des établissements pour le département cliqué
-    showMarkersForDepartment(deptName);
-}
-
-// Fonction pour attacher les événements de survol, de clic et de popup à chaque département
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
-
-// Chargement du fichier GeoJSON contenant tous les départements
-const geojsonUrl = "https://france-geojson.gregoiredavid.fr/repo/regions/ile-de-france/departements-ile-de-france.geojson";
-
-// Ajout du GeoJSON sur la carte avec le style conditionnel et événements
-const geojson = new L.GeoJSON.AJAX(geojsonUrl, {
-    style: highlightAcademy,
-    onEachFeature: onEachFeature
-}).addTo(map);
